ed9515fd6eb38ccafc0dd8b63e24d0bdaf64bf30
Jason Tieu <jt@Jasons-MacBook-Pro.local>
2018-12-29 11:58:17 -0600

    Added Passport JWT Authentication Strategy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:password1@ds243084.mlab.com:43084/devconnector"
+  mongoURI: "mongodb://admin:password1@ds243084.mlab.com:43084/devconnector",
+  secretOrKey: "secret"
 };
