0e414db5c9a93256d0c303ed565d0f7cd4739522
Jason Tieu <jt@Jasons-MacBook-Pro.local>
2018-12-31 14:51:22 -0600

    Added Post Model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,34 @@
 const express = require("express");
-
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
+// Post Model
+const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
 // @route       GET api/posts/test
 // @description Tests post route
 // @access      Public
 
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
+// @route       POST api/posts
+// @description Create post
+// @access      Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.name,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 module.exports = router;
